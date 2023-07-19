9571adaad911248721dd0093a3fce48cd9de8a55
rmfltm854 <rmfltm854@naver.com>
2023-07-19 21:52:01 +0900

    0719

---
+++ src/main/java/com/example/weathercodysummer/WeatherCodySummerApplication.java
@@ -0,0 +1,13 @@
+package com.example.weathercodysummer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherCodySummerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherCodySummerApplication.class, args);
+    }
+
+}
