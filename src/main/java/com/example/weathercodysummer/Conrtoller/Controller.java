83d1260aeed70b5e562ae14a3e1af5258583214a
Eun12341234 <shinje9511@naver.com>
2023-07-21 21:18:19 +0900

    신재은 로그인페이지 회원가입페이지 0721

--- src/main/java/com/example/weathercodysummer/Conrtoller/Controller.java
+++ src/main/java/com/example/weathercodysummer/Conrtoller/Controller.java
@@ -33,5 +33,15 @@ public class Controller {//윤서 등장
         return "product3madeByJms";
     }
 
+    @GetMapping("/Login")
+    public String loginPage(){
+        return "login";
+    }
+
+    @GetMapping("/SignUp")
+    public String signUp(){
+        return "signUp";
+    }
+
 
 }
